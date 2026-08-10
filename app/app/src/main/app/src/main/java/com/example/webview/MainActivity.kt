0bf85bc782d9dd6c46bc0c910182f9578cae0cd1
tubatuwa <www.shanto.official@gmail.com>
2026-08-10 20:02:30 +0600

    Update MainActivity.kt

--- app/app/src/main/app/src/main/java/com/example/webview/MainActivity.kt
+++ app/app/src/main/app/src/main/java/com/example/webview/MainActivity.kt
@@ -1,1 +1,22 @@
+package com.example.webview
 
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val webView = WebView(this)
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // ⬇️ আপনার ওয়েবসাইট লিংকটি এখানে বসাবেন
+        webView.loadUrl("https://tubatuwa.blogspot.com")
+
+        setContentView(webView)
+    }
+}
